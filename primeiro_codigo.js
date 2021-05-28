3d7799edd981bdf4c5339b4b7f83dc4e7194d670
Nathalia Alves <system.nathy@hotmail.com>
2021-05-28 15:42:09 -0400

    primeiro codigo

---
+++ primeiro_codigo.js
@@ -0,0 +1,13 @@
+console.log("hello world este é meu primeiro codigo typescript");
+console.log("================================================");
+var nome = "joao";
+console.log("ola " + nome + " seja bem vindo!");
+console.log("================================================");
+var Produto = /** @class */ (function () {
+    function Produto(produtoNome, produtoValor) {
+        this.nome = produtoNome;
+        this.nome = produtoValor;
+    }
+    return Produto;
+}());
+var playstation5 = new Produto("playstation 5 ", 5000);
---
+++ primeiro_codigo.ts
@@ -0,0 +1,17 @@
+console.log("hello world este é meu primeiro codigo typescript");
+
+console.log("================================================");
+const nome = "joao";
+console.log("ola " + nome + " seja bem vindo!");
+ console.log("================================================");
+
+ class Produto {
+     nome: string;
+     valor: number;
+     
+     constructor(produtoNome, produtoValor) {
+         this.nome = produtoNome;
+         this.nome = produtoValor;
+     }
+ }
+ const playstation5 = new Produto ("playstation 5 ", 5000);
