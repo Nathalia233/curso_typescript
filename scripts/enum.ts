09e681016c44105067971cf84ffc4ea8883b6929
Nathalia <System;.nathy@hotmail.com>
2021-06-04 15:46:53 -0400

    20.enum

---
+++ scripts/enum.ts
@@ -0,0 +1,8 @@
+enum Permission {
+    ADMIN = 1,
+    USER,
+    READONLY
+}
+console.log(Permission.ADMIN);
+console.log(Permission.USER);
+console.log(Permission.READONLY);
---
+++ scripts/object.ts
@@ -0,0 +1,10 @@
+ let configuraçaoServidor: object = {
+    servidor: "microsoft azure",
+    ip: "127.0.0.1",
+    senha: "654321"
+}
+
+configuraçaoServidor = {
+    servidor: "google cloud"
+}
+configuraçaoServidor = new Array();
