c166917de27465a67d01e96bf18b2cb6c7f4af0d
Nathalia <system.nathy@hotmail.com>
2021-06-14 15:12:09 -0400

    primeira classe

--- classes/primeira_classe.ts
+++ classes/primeira_classe.ts
@@ -8,10 +8,18 @@ class Pessoa {
         this.idade = idade;
         this.altura =  altura;
     }
-}
+    
 
+    toString(): string {
+
+return `a pessoa ${this.nome} tem ${this.idade} anos e mede ${this.altura} de altura `
+};
+}
 const ronaldo = new Pessoa ("ronaldo braz", 30, 1.85);
+const marcus = new Pessoa ("marcus ribeiro", 19, 2);
+
 
 ronaldo.nome = "ronaldo";
 
-console.log(ronaldo.nome);
+console.log("pessoa: " + ronaldo);
+console.log("pessoa: "+ marcus);
---
+++ output/classes/primeira_classe.js
@@ -0,0 +1,17 @@
+"use strict";
+class Pessoa {
+    constructor(nome, idade, altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+    toString() {
+        return `a pessoa ${this.nome} tem ${this.idade} anos e mede ${this.altura} de altura `;
+    }
+    ;
+}
+const ronaldo = new Pessoa("ronaldo braz", 30, 1.85);
+const marcus = new Pessoa("marcus ribeiro", 19, 2);
+ronaldo.nome = "ronaldo";
+console.log("pessoa: " + ronaldo);
+console.log("pessoa: " + marcus);
