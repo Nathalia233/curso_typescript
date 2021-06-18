fa0eb1cfecf94fb34824104a45d531b9c5c2a634
nathalia <system.nathy@hotmail.com>
2021-06-18 13:44:10 -0400

    classe

--- scripts/classes/classe_interface.ts
+++ scripts/classes/classe_interface.ts
@@ -6,7 +6,6 @@ interface IEmailv2{
 interface ITelefonev2 {
     nome: string,
     email: string,
-    telefone: number
 }
 interface INotificaçoesv2 {
     enviar(usuario: MeuUsuariov2): boolean;
@@ -46,17 +45,17 @@ class SMSv2 extends Notificaçoesv2 implements  INotificaçoesv2, ITelefonev2 {
 
     nome: string;
     email: string;
-    telefone: number;
+    numero: number | undefined;
 
     constructor(usuario: MeuUsuariov2){
         super() 
         this.nome = usuario.nome; 
         this.email = usuario.email;
-        this.telefone = usuario.telefone;
-}
+        this.numero = usuario.telefone;
+};
 
     enviar(usuario: MeuUsuariov2): boolean {
-        console.log(`enviando SMS para o usuario ${usuario.telefone}...`);
+        console.log(`enviando SMS para o usuario ${this.numero}...`);
         return true;
     }
 }
@@ -66,19 +65,14 @@ class Androidv2 extends Notificaçoesv2 {
         return true;
     }
 }
-/*new Emailv2().enviar({
+new Emailv2({
     nome: "joao",
     email: "joao@hcode.com.br",
     telefone: 119212345678
 });
-new SMSv2().enviar({
+new SMSv2({
     nome: "joao",
     email: "joao@hcode.com.br",
     telefone: 119212345678
 });
-new Androidv2().enviar({
-    nome: "joao",
-    email: "joao@hcode.com.br",
-    telefone: 119212345678,
-    idAndroid: "jhdsahdkhahsdkas"
-});*/
+
